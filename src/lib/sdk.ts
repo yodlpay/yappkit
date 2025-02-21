826bbdefaacf8f1eb067316df09b63c3d4a281cb
Andreas Oee <andy.oeee@gmail.com>
2025-02-21 16:34:00 +0700

    fix dependencies

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -6,8 +6,3 @@ export const sdk = new YappSDK({
   // origin: "http://localhost:3001",
   publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY, // ES256 PEM encoded public key
 });
-
-
-// origin: config.origin || 'https://yodl.me',
-// ensName: config.ensName,
-// publicKey: config.publicKey || YODL_PUBLIC_KEY,
