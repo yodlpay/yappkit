e0f2622ee33d444b9624f2a079e89cfa95849bc7
Andreas Oee <andy.oeee@gmail.com>
2025-02-22 16:06:05 +0700

    init super yapp poc url

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -4,7 +4,7 @@ console.log("🚀 process.env.NODE_ENV:", process.env.NODE_ENV);
 // Single instance that can be imported anywhere
 export const sdk = new YappSDK({
   ensName: "kitchensink-yapp.yodl.eth",
-  origin: "https://yodl.me",
-  // origin: process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://yodl.me",
+  // origin: "https://yodl.me",
+  origin: process.env.NODE_ENV === "development" ? "http://localhost:3001/" : "https://dapp-git-feat-superapp-poc-yodl.vercel.app/",
   publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY, // ES256 PEM encoded public key
 });
