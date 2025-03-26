35144ba4cfe78ee36dca0aed073af887b352ad60
Andreas Øe <andy.oeee@gmail.com>
2025-03-26 19:51:04 +0700

    Improvement(ui): user context and connect-read (#3)
    * improvement: user contact ui, show 0 balances
    
    * remove example values in user context hook

--- src/app/user/components/userInfoDisplay.tsx
+++ src/app/user/components/userInfoDisplay.tsx
@@ -42,7 +42,7 @@ export const UserInfoDisplay = () => {
       element: <CodeCopy text={address} truncate={true} />,
     },
     {
-      label: "Primary ENS",
+      label: "ENS (primary)",
       field: "primaryEnsName",
       description:
         "The connected ENS name of the user in the Yodl app. Useful for looking up ENS records, e.g. Yodl settings including preferred tokens and chains.",
@@ -50,20 +50,20 @@ export const UserInfoDisplay = () => {
     },
 
     {
-      label: "Community Address",
+      label: "Address",
       field: "community.address",
       description: "The address of the community.",
       element: <CodeCopy text={community?.address || "n/a"} truncate={true} />,
     },
     {
-      label: "Community ENS",
+      label: "ENS",
       field: "community.ensName",
       description:
         "The community from which the yapp was opened. Useful for implementing community specifc features, styling etc.",
       element: <CodeCopy text={community?.ensName || "n/a"} truncate={true} />,
     },
     {
-      label: "Community User ENS",
+      label: "ENS (user)",
       field: "community.userEnsName",
       description: "The ENS name of the user in the community.",
       element: <CodeCopy text={community?.userEnsName || "n/a"} truncate={true} />,
@@ -71,31 +71,46 @@ export const UserInfoDisplay = () => {
   ];
   return (
     <Card size="1">
-      <DataList.Root>
-        {userInfoDisplayItems.map((item, index) => (
-          <DataList.Item key={index} align="center">
-            <Popover.Root>
-              <Popover.Trigger>
-                <DataList.Label minWidth="88px">
-                  <Flex align="center" gap="1">
-                    <InfoCircledIcon color="teal" />
-                    {item.label}:
-                  </Flex>
-                </DataList.Label>
-              </Popover.Trigger>
-              <Popover.Content size="1" maxWidth="300px">
-                <Flex direction="column" gap="1">
-                  <Text size="1" color="gray">
-                    param: {item.field}
-                  </Text>
-                  <Text size="1">{item.description}</Text>
-                </Flex>
-              </Popover.Content>
-            </Popover.Root>
-            <DataList.Value>{item.element}</DataList.Value>
-          </DataList.Item>
-        ))}
-      </DataList.Root>
+      <Flex direction="column" gap="4">
+        <Text size="2" weight="bold" color="gray">
+          User
+        </Text>
+        <ItemList items={userInfoDisplayItems.slice(0, 2)} />
+        <Text size="2" weight="bold" color="gray">
+          Community
+        </Text>
+        <ItemList items={userInfoDisplayItems.slice(2)} />
+      </Flex>
     </Card>
+  );
+};
+
+export const ItemList = ({ items }: { items: UserInfoDisplayItem[] }) => {
+  return (
+    <DataList.Root>
+      {items.slice(0.3).map((item, index) => (
+        <DataList.Item key={index} align="center">
+          <Popover.Root>
+            <Popover.Trigger>
+              <DataList.Label minWidth="88px">
+                <Flex align="center" gap="1">
+                  <InfoCircledIcon color="teal" />
+                  <Text size="1">{item.label}:</Text>
+                </Flex>
+              </DataList.Label>
+            </Popover.Trigger>
+            <Popover.Content size="1" maxWidth="300px">
+              <Flex direction="column" gap="1">
+                <Text size="1" color="gray">
+                  param: {item.field}
+                </Text>
+                <Text size="1">{item.description}</Text>
+              </Flex>
+            </Popover.Content>
+          </Popover.Root>
+          <DataList.Value>{item.element}</DataList.Value>
+        </DataList.Item>
+      ))}
+    </DataList.Root>
   );
 };
